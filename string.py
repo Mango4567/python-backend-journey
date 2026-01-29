ac32b019c231cd8b70694718ad7a87ff3efddf85
Mango4567 <142215430+Mango4567@users.noreply.github.com>
2026-01-29 21:35:25 +0530

    learned basics of string

---
+++ string.py
@@ -0,0 +1,9 @@
+#concatenation of strings
+s=["thirty","days","of","python"]  #using '+' sign 
+concatenation= ["thirty"+' '+"days"+' '+"of"+' '+"python"]
+print(concatenation)  
+a,b,c=["Coding","for","all"] # using f-strings modern way
+print(f"{a}{b}{c}".lower()) #print()- only dispayes value but does not stores or assign value.
+print(a[0:6])
+arr="python","is","good"
+print("{},{},{}".format("python","is","good").upper()) #using .format method
