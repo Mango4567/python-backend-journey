a6b6dcfacbd273dcf2ec1817d67d6c846d1349c4
Mango4567 <142215430+Mango4567@users.noreply.github.com>
2026-01-31 12:04:45 +0530

    Add practice file for conditionals

---
+++ conditionals.py
@@ -0,0 +1,9 @@
+#first
+age=int(input("Enter your age:"))
+if(age)>=18:
+    print('You are old enough to learn to drive')
+elif(age)<18:
+    print("you need [18-age] to learn to drive")
+else:
+    print("wrong output")
+           
