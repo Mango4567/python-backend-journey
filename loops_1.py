b0f8d11fa0d9d0251fc6c2c534fc766b3b2128b9
Mango4567 <142215430+Mango4567@users.noreply.github.com>
2026-02-01 22:47:30 +0530

    learned loops basics

---
+++ loops_1.py
@@ -0,0 +1,36 @@
+#first
+for i in range(0,11,1): # imp- for while loop initialization,then condition,then update for any while while loop formation
+    print(i)
+#second
+c=0
+while (c)<11:
+    print(c)
+    c=c+1
+#third
+for i in range(10,-1,-1):
+    print(i)
+#fourth
+c=10
+while c>-1:
+    print(c)
+    c=c-1
+#fifth
+for i in range(1,11,2):
+    print(i)
+#sixth
+c=1
+while c<11:
+    print(c)
+    c=c+2
+#seventh
+for i in range(10,-1,-2):
+    print(i)
+#eighth
+c=10
+while c>=0:
+    print(c)
+    c=c-2
+ #ninth
+ for i in range(2,11):
+    if(i/1==i and i/i==1):
+        print(i)
